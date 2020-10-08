4dabe7a44bddfa37d5c85bf2602524497ca5fdda
Crowdbotics <team@crowdbotics.com>
2020-10-08 15:42:32 +0000

    Update components

--- src/features/BlankScreen011953/screens/blank.js
+++ src/features/BlankScreen011953/screens/blank.js
@@ -18,8 +18,27 @@ export class _Blank extends React.Component {
   static navigationOptions = ({ navigation }) => {
     return { headerLeft: <SlideMenuIcon navigationProps={navigation} /> }
   }
-  state = {}
-  render = () => <View style={styles.View_1} />
+  state = { TextInput_2: "", DateTimePicker_4: new Date("10/08/2020") }
+  render = () => (
+    <View style={styles.View_1}>
+      <TextInput
+        placeholder="Sample text input placeholder"
+        editable={true}
+        style={styles.TextInput_2}
+        value={this.state.TextInput_2}
+        onChangeText={nextValue => this.setState({ TextInput_2: nextValue })}
+      />
+      <Text style={styles.Text_3}>Sample text content</Text>
+      <DateTimePicker
+        showIcon={false}
+        style={styles.DateTimePicker_4}
+        date={this.state.DateTimePicker_4}
+        onDateChange={selectedDate =>
+          this.setState({ DateTimePicker_4: selectedDate })
+        }
+      />
+    </View>
+  )
 }
 
 Blank = withStyles(_Blank, theme => ({
